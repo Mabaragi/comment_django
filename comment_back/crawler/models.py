c77844c41610e2e750795b66c300f57996f94f0a
mabagit config --global user.name maba <gyj0149@gmail.com>
2024-12-06 15:14:56 +0900

    시리즈 모델 추가

--- comment_back/crawler/models.py
+++ comment_back/crawler/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from ..user.models import CustomUser
 
-# Create your models here.
+class Series(models.Model):  # models.Model을 상속해야 Django 모델로 인식됨
+    title = models.CharField(max_length=100)
+    image_src = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
